ff033c21becd240b9b2717de24d7b6ff974fe7d6
rahul_jangir <rahuljangirraj762000@gmail.com>
2024-06-19 18:11:57 +0530

    update

--- advance_password_generator.py
+++ advance_password_generator.py
@@ -1,7 +1,11 @@
 import random 
 
+# The code snippet you provided is generating a random password based on the user's input for the
+# number of letters, numbers, and symbols to include in the password.
 latters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+# The code snippet you provided is defining lists `numbers` and `symbols` which contain digits and
+# symbols respectively that can be used in the generated password.
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 symboles = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -12,6 +16,8 @@ n_symboles = int(input("how many symboles are you used in password : "))
 
 password_list = []
 
+# This part of the code snippet is responsible for generating the random password based on the user's
+# input for the number of letters, numbers, and symbols to include in the password.
 for i in range(1,n_latters+1):
     password_list += random.choice(latters)
 for i in range(1,n_number+1):
@@ -20,6 +26,9 @@ for i in range(1,n_symboles+1):
     password_list += random.choice(symboles)
 
 
+# The code snippet `random.shuffle(password_list)` shuffles the elements in the `password_list` list
+# in a random order. This means that the characters (letters, numbers, and symbols) in the
+# `password_list` will be rearranged randomly.
 random.shuffle(password_list)
 
 password = ""
